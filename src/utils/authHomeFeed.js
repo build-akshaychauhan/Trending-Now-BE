f7c57ab04f0fe1e477873714f22d7cf222e538ec
Lalit Apps <lalit.rathod@appsuccessor.com>
2026-08-03 18:32:00 +0530

    minor-fixes

--- src/utils/authHomeFeed.js
+++ src/utils/authHomeFeed.js
@@ -5,6 +5,25 @@ import SocialDumpStore from "../models/SocialDumpStore.js";
 import { collectPosts, StackPostMaker } from "./feedHelper.js";
 import { normaliseCreator } from "./normalizer.js";
 
+function getUniqueCreatorsWithHighestTopic(data) {
+  const creators = new Map();
+  if (!data) return [];
+
+  for (const item of data) {
+    const creator = item.creatorSlug;
+    const count = item.topTopics?.[0]?.count || 0;
+
+    if (
+      !creators.has(creator) ||
+      count > (creators.get(creator).topTopics?.[0]?.count || 0)
+    ) {
+      creators.set(creator, item);
+    }
+  }
+
+  return [...creators.values()];
+}
+
 export async function authHomeFeed(creator) {
   const topics = {};
 
@@ -55,13 +74,30 @@ export async function authHomeFeed(creator) {
 
   const PostStack = await StackPostMaker(creator.name, sortedTopics);
 
+  const BuzzingCards = getUniqueCreatorsWithHighestTopic(PostStack).map(
+    (item) => ({
+      id: `${item.creatorSlug}-${item.topicSlug}`,
+      creator: item?.creatorSlug?.replaceAll("_", " "),
+      topic: item.topicLabel,
+      count: item.topicCount,
+      headline: item.headline,
+      image:
+        item.igPost?.media?.[0]?.url ||
+        item.newsItems?.[0]?.urlToImage ||
+        item.twPost?.media?.[0]?.thumbnail ||
+        item.twPost?.media?.[0]?.url ||
+        item.shortPost?.thumbnailUrl,
+      type: item.stackCategory,
+    }),
+  );
+
   const headline =
-    sortedTopics[0].posts[0].normalizedText.length > 80
-      ? sortedTopics[0].posts[0].normalizedText.slice(0, 80) + "..."
-      : sortedTopics[0].posts[0].normalizedText;
+    sortedTopics[0]?.posts[0]?.normalizedText.length > 80
+      ? sortedTopics[0]?.posts[0]?.normalizedText.slice(0, 80) + "..."
+      : sortedTopics[0]?.posts[0]?.normalizedText;
 
   const topHeadline = sortedTopics[0]?.posts?.[0] && {
-    _id: sortedTopics[0].posts[0]._id || sortedTopics[0].posts[0].id,
+    _id: sortedTopics[0]?.posts[0]?._id || sortedTopics[0]?.posts[0]?.id,
     headline: headline,
   };
 
@@ -77,5 +113,6 @@ export async function authHomeFeed(creator) {
     topHeadline: topHeadline,
     topicSlug: topicSlug,
     PostStack: PostStack,
+    BuzzingCards: BuzzingCards,
   };
 }
